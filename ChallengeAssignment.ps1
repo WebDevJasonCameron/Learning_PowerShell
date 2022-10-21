4e084b5b8c2dd1aa576ca451b5e0badd2a1de50e
dev.jasoncameron <dev.jasoncameron@gmail.com>
2022-10-20 21:06:44 -0500

    comp challenge for var

---
+++ ChallengeAssignment.ps1
@@ -0,0 +1,11 @@
+﻿# Read user input
+$FirstName = Read-Host "Please enter your first name"
+$LastName = Read-Host "Please enter your last name"
+
+# Get today's date with propper formatting
+$Today = Get-Date -Format "ddMMyyyy-HHMMs"
+$LongDate = Get-Date "F"
+
+
+# Write this information to the file
+Set-Content -Value "..." -Path "C:\Users\jasoncameron\Desktop\powershellStudy\$today-$FirstName-$LastName.txt"
