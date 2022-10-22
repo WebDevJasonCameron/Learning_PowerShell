ce9bf7bf4030d793181e25ef0a8bddaa70ff08b4
dev.jasoncameron <dev.jasoncameron@gmail.com>
2022-10-21 21:40:47 -0500

    comp while loop lsn

---
+++ whileLoopLsn.ps1
@@ -0,0 +1,31 @@
+﻿# Wile Loops Lsn
+
+# Example 1
+$i = 0
+while($i -lt 5) {
+    $i
+    $i++
+}
+
+# Example 2
+while((Read-Host "Type 'xyz' to stop the loop") -ne "xyz") {
+    echo "You did not type 'xyz' so I am going to continue the loop"
+}
+
+# Example 3
+while($userInput -ne "xyz") {
+    $userInput = Read-Host "Please type 'xyz' to stop the loop"
+}
+$userInput = $null
+
+
+# Example 4
+while($userInput -ne "quit"){
+    # Gather user input
+    $userInput = Read-Host "Say 'yes' if you like scripting (enter 'quit' to stop the loop)"
+
+    # Evaluate user input
+    if($userInput -eq "yes") { echo "I also love scripting! That's cool" }
+    elseif ($userInput -eq "no") { echo "I really hate scripting too" }
+}
+$userInput = $null
