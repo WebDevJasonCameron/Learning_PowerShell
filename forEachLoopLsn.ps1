7588afd022f47eff7f5955ff579663aa94f4f1bd
dev.jasoncameron <dev.jasoncameron@gmail.com>
2022-10-23 14:12:19 -0500

    comp foreach loop lsn

---
+++ forEachLoopLsn.ps1
@@ -0,0 +1,11 @@
+﻿# For Each Loop
+$Vehicles = @("Cars", "Bikes", "Trucks", "SUVs")
+for ($i=0; $i -lt $Vehicles.Count; $i++){
+    echo ("Element $i = " + $Vehicles[$i])
+}
+
+
+$myArray = @("E1", "E2")
+ForEach ($E in $myArray) {
+    $E
+}
