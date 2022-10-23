90e2d4ebac2234bfa53b876b3f75abd6257e5c78
dev.jasoncameron <dev.jasoncameron@gmail.com>
2022-10-23 14:34:36 -0500

    comp conditional statements lsn

---
+++ breakLsn.ps1
@@ -0,0 +1,14 @@
+﻿# Break Lsn (out of the loop)
+
+$a = @("Honda", "Toyota", "Ford", "Chevy")
+foreach ($e in $a){
+    if ($e -eq "Ford") {break}
+    $e
+}
+
+# Continue Lsn (skip and continue with loop)
+$a = @("Honda", "Toyota", "Ford", "Chevy")
+foreach ($e in $a){
+    if ($e -eq "Ford") {continue}
+    $e
+}
---
+++ conditionalStatementsLsn.ps1
@@ -0,0 +1,65 @@
+﻿# Conditional Statements
+if (1 -eq 2) { 
+    echo "I like hotdogs" 
+} else { 
+    echo "I dislike hotdogs" 
+}
+
+$v1 = 1
+$v2 = 0
+if ($v1 -eq $v2) { 
+    echo "I like hotdogs" 
+} else { 
+    echo "I dislike hotdogs" 
+}
+
+if (1 -gt 2) { 
+    echo "I like hotdogs" 
+} else { 
+    echo "I dislike hotdogs" 
+}
+
+# With user input
+$c = Read-Host "Please select an option (1-3)"
+
+if ($c -eq 1) {
+    echo "You chose option 1"
+} elseif ($c -eq 2) {
+    echo "You chose 2"
+} elseif ($c -eq 3) {
+    echo "You chose 3"
+} else {
+    echo "You did not choose a valid option"
+}
+
+
+# Switch
+switch (Read-Host "Please select an option (1-3)"){
+    1 {"YOu chose option 1"}
+    2 {"YOu chose option 2"}        
+    3 {"YOu chose option 3"}
+    default {"You did not chose a valid option"}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
